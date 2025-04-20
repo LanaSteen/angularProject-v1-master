2451aebee4a63b458318a0b3db548e4803b99d03
Shako The developer Giorgashvili <150827256+shako03@users.noreply.github.com>
2025-04-20 21:20:13 +0400

    msg

--- src/app/CARDS-HOTEL/rooms-card/rooms-card.component.ts
+++ src/app/CARDS-HOTEL/rooms-card/rooms-card.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Room } from '../../models/rooms';
 import { RouterLink, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-rooms-card',
-  imports: [RouterModule,RouterLink],
+  imports: [RouterModule,RouterLink,FormsModule,CommonModule],
   templateUrl: './rooms-card.component.html',
   styleUrl: './rooms-card.component.scss'
 })
